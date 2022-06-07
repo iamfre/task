4bc5a98a71ab0a91a4e083a7a4f64392793a78b0
iamfre <zycs@yandex.ru>
2022-06-07 19:57:50 +0500

    Relationships

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Group extends Model
 {
     use HasFactory;
     public $timestamps = false;
     protected $table = 'groups';
+
+    /**
+     * Пользователи, принадлежащие к группам.
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Model
 {
     use HasFactory;
     public $timestamps = false;
     protected $table = 'users';
+
+    /**
+     * Группы, принадлежащие пользователю.
+     */
+    public function groups(): BelongsToMany
+    {
+        return $this->belongsToMany(Group::class);
+    }
 }
